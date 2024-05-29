47d8d579643dc452683c6a880fdb857e77a2cfed
dottox <imsm2424@gmail.com>
2024-05-29 18:12:21 -0300

    feat: lab-141

---
+++ codigos/python/25-05/ejer4.py
@@ -0,0 +1,24 @@
+# --- Ejercicio 1 ---
+# def info():
+#   nombre = input("Nombre: ")
+#   apellido = input("Apellido: ")
+#   edad = int(input("Edad: "))
+#   nacionalidad = input("Nacionalidad: ")
+
+#   print("Información:")
+#   print("Nombre:", nombre)
+#   print("Apellido:", apellido)
+#   print("Edad:", edad)
+#   print("Nacionalidad:", nacionalidad)
+
+# info()
+
+# --- Ejercicio 2 ---
+# Convertir string a caracteres ASCII
+# def ascii(texto):
+#   for i in texto:
+#     print(ord(i), end=" ")
+#   print()
+
+# texto = input("Texto: ")
+# ascii(texto)
